2b34321c6ee84c53e055f4a3336aa7682a14b8b4
root <root@virtualserver01.W251.cloud>
2020-02-02 21:40:09 +0000

    Got it running finally

--- hw3/Cloud_Files/faces_subscribe.py
+++ hw3/Cloud_Files/faces_subscribe.py
@@ -3,45 +3,51 @@ import numpy as np
 import cv2 as cv
 import sys
 
-LOCAL_MQTT_HOST="test.mosquitto.org"
-LOCAL_MQTT_PORT=1883
-LOCAL_MQTT_TOPIC="mb_faces/#"
+MQTT_HOST="test.mosquitto.org"
+MQTT_PORT=1883
+MQTT_TOPIC="mb_faces/#"
 
-OUTPUT_DIR = '/HW03/FACES/'
+OUTPUT_DIR = '/HW03/Faces'
 
-global img_num = 0
+img_num = 0
 
 def on_connect_local(client, userdata, flags, rc):
-        print("connected to local broker with rc: " + str(rc))
-        client.subscribe(LOCAL_MQTT_TOPIC)
+        print("Connected to broker with rc: " + str(rc))
+        client.subscribe(MQTT_TOPIC)
 
 def on_message(client,userdata, msg):
+  global img_num
   try:
-    print("message received!".format(msg))
-    # if we wanted to re-publish this message, something like this should work
-    # msg = msg.payload
-    face = cv.imdecode(msg.payload.decode(), cv.COLOR_BGR2GRAY) 
-    
+    print("message received!")
+    img = np.fromstring(msg.payload, dtype='uint8')
+    img = cv.imdecode(img, cv.IMREAD_GRAYSCALE)
+
+    # print shape to ensure we received a reasonable size image
+    # print('img shape:', img.shape)
     # write face to local director to ensure it's properly receiving
+
     if(img_num <10):
       img_name = OUTPUT_DIR + "/face-0" + str(img_num) + ".png"
       print(img_name)
     else:
       img_name = OUTPUT_DIR + "/face-" + str(img_num) + ".png"
-      print(img_name)
-    cv.imwrite(img_name, face)
-    # remote_mqttclient.publish(REMOTE_MQTT_TOPIC, payload=msg, qos=0, retain=False)
+      print("image name:",img_name)
+    
+    print('Writing File ...')
+    cv.imwrite(img_name, img)
+    print('Finished Writing File')
+
     
     img_num += 1
   except:
     print("Unexpected error:", sys.exc_info()[0])
 
-local_mqttclient = mqtt.Client()
-local_mqttclient.on_connect = on_connect_local
-local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
-local_mqttclient.on_message = on_message
+mqttclient = mqtt.Client()
+mqttclient.on_connect = on_connect_local
+mqttclient.connect(MQTT_HOST, MQTT_PORT, 60)
+mqttclient.on_message = on_message
 
 
 
 # go into a loop
-local_mqttclient.loop_forever()
+mqttclient.loop_forever()
