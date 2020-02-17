a62b415f5ae0efe78350b81b56502e1367d9607f
Matt Brimmer <mbrimmer@berkeley.edu>
2020-02-16 21:06:51 -0500

    Added dockerface.sh file needed components

--- hw7/Jetson_Files/DockerFace_hw7.sh
+++ hw7/Jetson_Files/DockerFace_hw7.sh
@@ -8,4 +8,4 @@ docker run \
 --volume="/etc/shadow:/etc/shadow:ro" \
 --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
 --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
---name mb_face_app --privileged --network hw03 -v "$PWD"/..:/HW03 -ti ubuntu_jtx2 bash
+--name mb_face_app_hw7 --privileged --rm -p 8888:8888 -v "$PWD":/HW07 -it hw7_img bash
