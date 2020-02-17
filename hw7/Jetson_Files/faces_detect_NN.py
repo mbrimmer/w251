488792a58ddf63bbf4e3ff04c8ba25506fc55d3d
Matt Brimmer <mbrimmer@berkeley.edu>
2020-02-16 21:25:13 -0500

    testing

--- hw7/Jetson_Files/faces_detect_NN.py
+++ hw7/Jetson_Files/faces_detect_NN.py
@@ -13,7 +13,115 @@ import sys
 
 DEBUG=True
 
+""""""""EVENTUALLY PULL THIS INTO Dockerfile"""""""""""
+# https://github.com/yeephycho/tensorflow-face-detection
+FROZEN_GRAPH_NAME = 'data/frozen_inference_graph_face.pb'
+!wget https://github.com/yeephycho/tensorflow-face-detection/blob/master/model/frozen_inference_graph_face.pb?raw=true -O {FROZEN_GRAPH_NAME}
 
+IMAGE_PATH = 'data/warriors.jpg'
+!wget 'https://cdn.vox-cdn.com/thumbor/rC0mlBATZdoDW1tEa44P6431sGc=/0x0:3683x2455/1200x800/filters:focal(1623x234:2211x822)/cdn.vox-cdn.com/uploads/chorus_image/image/63273148/usa_today_12005182.0.jpg' -O {IMAGE_PATH}
+
+output_dir=''
+frozen_graph = tf.GraphDef()
+with open(os.path.join(output_dir, FROZEN_GRAPH_NAME), 'rb') as f:
+  frozen_graph.ParseFromString(f.read())
+
+# https://github.com/NVIDIA-AI-IOT/tf_trt_models/blob/master/tf_trt_models/detection.py
+INPUT_NAME='image_tensor'
+BOXES_NAME='detection_boxes'
+CLASSES_NAME='detection_classes'
+SCORES_NAME='detection_scores'
+MASKS_NAME='detection_masks'
+NUM_DETECTIONS_NAME='num_detections'
+
+input_names = [INPUT_NAME]
+output_names = [BOXES_NAME, CLASSES_NAME, SCORES_NAME, NUM_DETECTIONS_NAME]
+
+trt_graph = trt.create_inference_graph(
+    input_graph_def=frozen_graph,
+    outputs=output_names,
+    max_batch_size=1,
+    max_workspace_size_bytes=1 << 25,
+    precision_mode='FP16',
+    minimum_segment_size=50
+)
+
+
+tf_config = tf.ConfigProto()
+tf_config.gpu_options.allow_growth = True
+
+tf_sess = tf.Session(config=tf_config)
+
+# use this if you want to try on the optimized TensorRT graph
+# Note that this will take a while
+# tf.import_graph_def(trt_graph, name='')
+
+# use this if you want to try directly on the frozen TF graph
+# this is much faster
+tf.import_graph_def(frozen_graph, name='')
+
+tf_input = tf_sess.graph.get_tensor_by_name(input_names[0] + ':0')
+tf_scores = tf_sess.graph.get_tensor_by_name('detection_scores:0')
+tf_boxes = tf_sess.graph.get_tensor_by_name('detection_boxes:0')
+tf_classes = tf_sess.graph.get_tensor_by_name('detection_classes:0')
+tf_num_detections = tf_sess.graph.get_tensor_by_name('num_detections:0')
+
+image = Image.open(IMAGE_PATH)
+
+plt.imshow(image)
+
+image_resized = np.array(image.resize((300, 300)))
+image = np.array(image)
+
+time.sleep(5)
+
+scores, boxes, classes, num_detections = tf_sess.run([tf_scores, tf_boxes, tf_classes, tf_num_detections], feed_dict={
+    tf_input: image_resized[None, ...]
+})
+
+boxes = boxes[0] # index by 0 to remove batch dimension
+scores = scores[0]
+classes = classes[0]
+num_detections = num_detections[0]
+
+# suppress boxes that are below the threshold..
+DETECTION_THRESHOLD = 0.5
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+ax.imshow(image)
+
+# plot boxes exceeding score threshold
+for i in range(int(num_detections)):
+    if scores[i] < DETECTION_THRESHOLD:
+        continue
+    # scale box to image coordinates
+    box = boxes[i] * np.array([image.shape[0], image.shape[1], image.shape[0], image.shape[1]])
+
+    # display rectangle
+    patch = patches.Rectangle((box[1], box[0]), box[3] - box[1], box[2] - box[0], color='g', alpha=0.3)
+    ax.add_patch(patch)
+
+    # display class index and score
+    plt.text(x=box[1] + 10, y=box[2] - 10, s='%d (%0.2f) ' % (classes[i], scores[i]), color='w')
+
+plt.show()
+
+time.sleep(15)
+
+num_samples = 50
+
+t0 = time.time()
+for i in range(num_samples):
+    scores, boxes, classes, num_detections = tf_sess.run([tf_scores, tf_boxes, tf_classes, tf_num_detections], feed_dict={
+        tf_input: image_resized[None, ...]
+    })
+t1 = time.time()
+print('Average runtime: %f seconds' % (float(t1 - t0) / num_samples))
+
+
+time.sleep(30)
 
 cap = cv.VideoCapture(1)
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
