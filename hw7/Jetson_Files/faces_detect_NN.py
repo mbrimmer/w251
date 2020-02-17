06ee4f7fae2dd4e6ceed145058263d866e88ac89
Matt Brimmer <mbrimmer@berkeley.edu>
2020-02-16 23:11:57 -0500

    nn attempt 1

--- hw7/Jetson_Files/faces_detect_NN.py
+++ hw7/Jetson_Files/faces_detect_NN.py
@@ -76,66 +76,66 @@ tf_boxes = tf_sess.graph.get_tensor_by_name('detection_boxes:0')
 tf_classes = tf_sess.graph.get_tensor_by_name('detection_classes:0')
 tf_num_detections = tf_sess.graph.get_tensor_by_name('num_detections:0')
 
-image = Image.open(IMAGE_PATH)
-
-print('************************** IMAGE **************')
-plt.imshow(image)
-plt.show()
-print('************************** IMAGE **************')
-
-image_resized = np.array(image.resize((300, 300)))
-image = np.array(image)
-
-time.sleep(5)
-
-scores, boxes, classes, num_detections = tf_sess.run([tf_scores, tf_boxes, tf_classes, tf_num_detections], feed_dict={
-    tf_input: image_resized[None, ...]
-})
-
-boxes = boxes[0] # index by 0 to remove batch dimension
-scores = scores[0]
-classes = classes[0]
-num_detections = num_detections[0]
-
-# suppress boxes that are below the threshold..
-DETECTION_THRESHOLD = 0.5
-
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-
-ax.imshow(image)
-
-
-# plot boxes exceeding score threshold
-for i in range(int(num_detections)):
-    if scores[i] < DETECTION_THRESHOLD:
-        continue
-    # scale box to image coordinates
-    box = boxes[i] * np.array([image.shape[0], image.shape[1], image.shape[0], image.shape[1]])
-
-    # display rectangle
-    patch = patches.Rectangle((box[1], box[0]), box[3] - box[1], box[2] - box[0], color='g', alpha=0.3)
-    ax.add_patch(patch)
-
-    # display class index and score
-    plt.text(x=box[1] + 10, y=box[2] - 10, s='%d (%0.2f) ' % (classes[i], scores[i]), color='w')
-
-plt.show()
-
-time.sleep(15)
-
-num_samples = 50
-
-t0 = time.time()
-for i in range(num_samples):
-    scores, boxes, classes, num_detections = tf_sess.run([tf_scores, tf_boxes, tf_classes, tf_num_detections], feed_dict={
-        tf_input: image_resized[None, ...]
-    })
-t1 = time.time()
-print('Average runtime: %f seconds' % (float(t1 - t0) / num_samples))
-
-
-time.sleep(30)
+# image = Image.open(IMAGE_PATH)
+#
+# print('************************** IMAGE **************')
+# plt.imshow(image)
+# plt.show()
+# print('************************** IMAGE **************')
+#
+# image_resized = np.array(image.resize((300, 300)))
+# image = np.array(image)
+#
+# time.sleep(5)
+#
+# scores, boxes, classes, num_detections = tf_sess.run([tf_scores, tf_boxes, tf_classes, tf_num_detections], feed_dict={
+#     tf_input: image_resized[None, ...]
+# })
+#
+# boxes = boxes[0] # index by 0 to remove batch dimension
+# scores = scores[0]
+# classes = classes[0]
+# num_detections = num_detections[0]
+#
+# # suppress boxes that are below the threshold..
+# DETECTION_THRESHOLD = 0.5
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(1, 1, 1)
+#
+# ax.imshow(image)
+#
+#
+# # plot boxes exceeding score threshold
+# for i in range(int(num_detections)):
+#     if scores[i] < DETECTION_THRESHOLD:
+#         continue
+#     # scale box to image coordinates
+#     box = boxes[i] * np.array([image.shape[0], image.shape[1], image.shape[0], image.shape[1]])
+#
+#     # display rectangle
+#     patch = patches.Rectangle((box[1], box[0]), box[3] - box[1], box[2] - box[0], color='g', alpha=0.3)
+#     ax.add_patch(patch)
+#
+#     # display class index and score
+#     plt.text(x=box[1] + 10, y=box[2] - 10, s='%d (%0.2f) ' % (classes[i], scores[i]), color='w')
+#
+# plt.show()
+#
+# time.sleep(15)
+#
+# num_samples = 50
+#
+# t0 = time.time()
+# for i in range(num_samples):
+#     scores, boxes, classes, num_detections = tf_sess.run([tf_scores, tf_boxes, tf_classes, tf_num_detections], feed_dict={
+#         tf_input: image_resized[None, ...]
+#     })
+# t1 = time.time()
+# print('Average runtime: %f seconds' % (float(t1 - t0) / num_samples))
+#
+#
+# time.sleep(30)
 
 cap = cv.VideoCapture(1)
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -148,19 +148,36 @@ while(True):
     # Capture frame-by-frame from feed
     ret, frame = cap.read()
 
+    image_resized = np.array(frame.resize((300, 300)))
+
+    scores, boxes, classes, num_detections = tf_sess.run([tf_scores, tf_boxes, tf_classes, tf_num_detections],
+        feed_dict={
+            tf_input: image_resized[None, ...]
+            })
+
     # gray here is the gray frame from camera
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
+
+    DETECTION_THRESHOLD = 0.5
     # Display image, faces, and publish message
-    img = cv.imshow('frame', gray)
-    for (x,y,w,h) in faces:
-        crop_faces = gray[y:y+h,x:x+w]
-        cv.imshow("crop", crop_faces)
-        # Publish coordinates (debug)
-        coord_payload = str(img_num)+ ':' + ' (' + str(x) + "," + str(y) + ')'
+    img = cv.imshow('frame', frame)
+    #img = cv.imshow('frame', gray)
+    for i in range(int(num_detections)):
+        if scores[i] < DETECTION_THRESHOLD:
+            continue
+
         if DEBUG:
-            print(f"Image: {img_num}, payload={coord_payload} sent...")
+            print(f"Image: y_min={boxes[i][0]}, y_max={boxes[i][2]}, x_min={boxes[i][1]}, x_max={boxes[i][3]}")
+        # scale box to image coordinates
+        # box = boxes[i] * np.array([image.shape[0], image.shape[1], image.shape[0], image.shape[1]])
+
+        # crop_faces = gray[y:y+h,x:x+w]
+        # cv.imshow("crop", crop_faces)
+        # Publish coordinates (debug)
+        # coord_payload = str(img_num)+ ':' + ' (' + str(x) + "," + str(y) + ')'
+
         img_num+=1
 
     # Close the connection
